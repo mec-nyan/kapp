84dbb7563bc000e4df46a68a5fe0f7e35a9a661a
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-10 21:02:41 -0300

    Adapt layout to new components.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,14 @@
 import './home.scss'
-import Card from '../features/cards/components/Card'
 import TopBar from '../features/shared/components/topBar'
 import Footer from '../features/shared/components/footer'
+import MainMenu from '../features/home/components/mainMenu'
 
 export function Home() {
   return (
     <div id='main-container'>
       <TopBar />
 
-      <Card
-        hiragana='も'
-        katakana='モ'
-        picture='🍑'
-        wordInHiragana='もも'
-        wordInRomaji='momo'
-      />
+      <MainMenu />
 
       <Footer />
     </div>
