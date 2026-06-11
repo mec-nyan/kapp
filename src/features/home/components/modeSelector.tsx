32d6228d397cc63e5d5cf2c225eccda8e07b8abd
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-10 21:33:24 -0300

    Add styles and layout for mode selector.

--- src/features/home/components/modeSelector.tsx
+++ src/features/home/components/modeSelector.tsx
@@ -3,8 +3,18 @@ import './modeSelector.scss'
 export default function ModeSelector() {
   return (
     <div className='mode-selector'>
-      <div className='mode-hiragana'>Hiragana</div>
-      <div className='mode-katakana active'>Katakana</div>
+      <div className='mode-hiragana-outer'>
+        <div className='mode-hiragana'>
+          <div className='label-in-kana'>ひらがな</div>
+          <div className='label-in-romaji'>hiragana</div>
+        </div>
+      </div>
+      <div className='mode-katakana-outer active'>
+        <div className='mode-katakana active'>
+          <div className='label-in-kana'>カタカナ</div>
+          <div className='label-in-romaji'>katakana</div>
+        </div>
+      </div>
     </div>
   )
 }
