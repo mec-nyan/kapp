24c202a8f6a0ca8a7bc729b3038c037209027247
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-10 22:14:05 -0300

    Add state + animation to mode selector.

--- src/features/home/components/mainMenu.tsx
+++ src/features/home/components/mainMenu.tsx
@@ -1,10 +1,14 @@
-import ModeSelector from "./modeSelector";
+import ModeSelector from './modeSelector'
 import './mainMenu.scss'
+import { useState } from 'preact/hooks'
+import type { kanaKind } from '../../../types/kana'
 
 export default function MainMenu() {
+  const [kana, setKana] = useState<kanaKind>('hiragana')
+
   return (
     <div className='main-menu'>
-      <ModeSelector />
+      <ModeSelector kana={kana} setKana={setKana} />
     </div>
   )
 }
--- src/features/home/components/modeSelector.tsx
+++ src/features/home/components/modeSelector.tsx
@@ -1,16 +1,36 @@
+import type { kanaKind } from '../../../types/kana'
 import './modeSelector.scss'
 
-export default function ModeSelector() {
+export default function ModeSelector({
+  kana,
+  setKana,
+}: {
+  kana: kanaKind
+  setKana: (k: kanaKind) => void
+}) {
   return (
     <div className='mode-selector'>
-      <div className='mode-hiragana-outer'>
-        <div className='mode-hiragana'>
+      <div
+        className={`mode-outer-gradient ${kana === 'hiragana' ? 'left' : 'right'}`}
+      >
+        <div></div>
+      </div>
+
+      <div
+        className={`mode-hiragana-outer ${kana === 'hiragana' && 'active'}`}
+        onClick={() => setKana('hiragana')}
+      >
+        <div className={`mode-hiragana ${kana === 'hiragana' && 'active'}`}>
           <div className='label-in-kana'>ひらがな</div>
           <div className='label-in-romaji'>hiragana</div>
         </div>
       </div>
-      <div className='mode-katakana-outer active'>
-        <div className='mode-katakana active'>
+
+      <div
+        className={`mode-katakana-outer ${kana === 'katakana' && 'active'}`}
+        onClick={() => setKana('katakana')}
+      >
+        <div className={`mode-katakana ${kana === 'katakana' && 'active'}`}>
           <div className='label-in-kana'>カタカナ</div>
           <div className='label-in-romaji'>katakana</div>
         </div>
