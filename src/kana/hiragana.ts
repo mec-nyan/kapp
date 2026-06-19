cc74b87b0c188db90c31ac1d1540b2620717be5c
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-19 18:46:12 -0300

    Add hiragana constants.

---
+++ src/kana/hiragana.ts
@@ -0,0 +1,157 @@
+export default {
+  // 五十音（ごじゅうおん）Monographs.
+  a: 'あ',
+  i: 'い',
+  u: 'う',
+  e: 'え',
+  o: 'お',
+
+  ka: 'か',
+  ki: 'き',
+  ku: 'く',
+  ke: 'け',
+  ko: 'こ',
+
+  sa: 'さ',
+  si: 'し',
+  su: 'す',
+  se: 'せ',
+  so: 'そ',
+
+  ta: 'た',
+  ti: 'ち',
+  tu: 'つ',
+  te: 'て',
+  to: 'と',
+
+  na: 'な',
+  ni: 'に',
+  nu: 'ぬ',
+  ne: 'ね',
+  no: 'の',
+
+  ha: 'は',
+  hi: 'ひ',
+  hu: 'ふ',
+  he: 'へ',
+  ho: 'ほ',
+
+  ma: 'ま',
+  mi: 'み',
+  mu: 'む',
+  me: 'め',
+  mo: 'も',
+
+  ya: 'や',
+  yu: 'ゆ',
+  yo: 'よ',
+
+  ra: 'ら',
+  ri: 'り',
+  ru: 'る',
+  re: 'れ',
+  ro: 'ろ',
+
+  wa: 'わ',
+  wo: 'を',
+  n: 'ん',
+
+  // 濁点（だくてん）dakuten (diacritics).
+  ga: 'が',
+  gi: 'ぎ',
+  gu: 'ぐ',
+  ge: 'げ',
+  go: 'ご',
+
+  za: 'ざ',
+  zi: 'じ',
+  zu: 'ず',
+  ze: 'ぜ',
+  zo: 'ぞ',
+
+  da: 'だ',
+  di: 'ぢ',
+  du: 'づ',
+  de: 'で',
+  do: 'ど',
+
+  ba: 'ば',
+  bi: 'び',
+  bu: 'ぶ',
+  be: 'べ',
+  bo: 'ぼ',
+
+  // 半濁点　（はんだくてん）handakuten (diacritics)
+  pa: 'ぱ',
+  pi: 'ぴ',
+  pu: 'ぷ',
+  pe: 'ぺ',
+  po: 'ぽ',
+
+  // Soma aliases for other common romaji forms.
+  // Warning: Some conflicting forms are not being included.
+  // i.e. "ji" and "zu"
+  shi: 'し',
+  chi: 'ち',
+  tsu: 'つ',
+  fu: 'ふ',
+  ji: 'じ',
+
+  // Digraphs.
+  // 五十音（ごじゅうおん）
+  kya: 'きゃ',
+  kyu: 'きゅ',
+  kyo: 'きょ',
+
+  // Usually romanised as "sha, shu, sho"
+  sya: 'しゃ',
+  syu: 'しゅ',
+  syo: 'しょ',
+
+  // Usually romanised as "cha, chu, cho"
+  tya: 'ちゃ',
+  tyu: 'ちゅ',
+  tyo: 'ちょ',
+
+  nya: 'にゃ',
+  nyu: 'にゅ',
+  nyo: 'にょ',
+
+  hya: 'ひゃ',
+  hyu: 'ひゅ',
+  hyo: 'ひょ',
+
+  mya: 'みゃ',
+  myu: 'みゅ',
+  myo: 'みょ',
+
+  rya: 'りゃ',
+  ryu: 'りゅ',
+  ryo: 'りょ',
+
+  // 濁点（だくてん）dakuten (diacritics).
+  gya: 'ぎゃ',
+  gyu: 'ぎゅ',
+  gyo: 'ぎょ',
+
+  // Usually romanised as "ja, ju, jo"
+  zya: 'じゃ',
+  zyu: 'じゅ',
+  zyo: 'じょ',
+
+  // Usually also romanised as "ja, ju, jo"
+  dya: 'ちゃ',
+  dyu: 'ちゅ',
+  dyo: 'ちょ',
+
+  bya: 'びゃ',
+  byu: 'びゅ',
+  byo: 'びょ',
+
+  // 半濁点　（はんだくてん）handakuten (diacritics)
+  pya: 'ぴゃ',
+  pyu: 'ぴゅ',
+  pyo: 'ぴょ',
+
+  // End Hiragana.
+} as const
