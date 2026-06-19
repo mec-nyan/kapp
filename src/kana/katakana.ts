56989676f03b726888a12cb3d8ad6f11016646e2
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-19 18:50:45 -0300

    Add katakana constants.

---
+++ src/kana/katakana.ts
@@ -0,0 +1,157 @@
+export default {
+  // 五十音（ごじゅうおん）Monographs.
+  a: 'ア',
+  i: 'イ',
+  u: 'ウ',
+  e: 'エ',
+  o: 'オ',
+
+  ka: 'カ',
+  ki: 'キ',
+  ku: 'ク',
+  ke: 'ケ',
+  ko: 'コ',
+
+  sa: 'サ',
+  si: 'シ',
+  su: 'ス',
+  se: 'セ',
+  so: 'ソ',
+
+  ta: 'タ',
+  ti: 'チ',
+  tu: 'ツ',
+  te: 'テ',
+  to: 'ト',
+
+  na: 'ナ',
+  ni: 'ニ',
+  nu: 'ヌ',
+  ne: 'ネ',
+  no: 'ノ',
+
+  ha: 'ハ',
+  hi: 'ヒ',
+  hu: 'フ',
+  he: 'ヘ',
+  ho: 'ホ',
+
+  ma: 'マ',
+  mi: 'ミ',
+  mu: 'ム',
+  me: 'メ',
+  mo: 'モ',
+
+  ya: 'ヤ',
+  yu: 'ユ',
+  yo: 'ヨ',
+
+  ra: 'ラ',
+  ri: 'リ',
+  ru: 'ル',
+  re: 'レ',
+  ro: 'ロ',
+
+  wa: 'ワ',
+  wo: 'ヲ',
+  n: 'ン',
+
+  // 濁点（だくてん）dakuten (diacritics).
+  ga: 'ガ',
+  gi: 'ギ',
+  gu: 'グ',
+  ge: 'ゲ',
+  go: 'ゴ',
+
+  za: 'ザ',
+  zi: 'ジ',
+  zu: 'ズ',
+  ze: 'ゼ',
+  zo: 'ゾ',
+
+  da: 'ダ',
+  di: 'ヂ',
+  du: 'ヅ',
+  de: 'デ',
+  do: 'ド',
+
+  ba: 'バ',
+  bi: 'ビ',
+  bu: 'ブ',
+  be: 'ベ',
+  bo: 'ボ',
+
+  // 半濁点　（はんだくてん）handakuten (diacritics)
+  pa: 'パ',
+  pi: 'ピ',
+  pu: 'プ',
+  pe: 'ペ',
+  po: 'ポ',
+
+  // Soma aliases for other common romaji forms.
+  // Warning: Some conflicting forms are not being included.
+  // i.e. "ji" and "zu"
+  shi: 'シ',
+  chi: 'チ',
+  tsu: 'ツ',
+  fu: 'フ',
+  ji: 'ジ',
+
+  // Digraphs.
+  // 五十音（ごじゅうおん）
+  kya: 'キャ',
+  kyu: 'キュ',
+  kyo: 'キョ',
+
+  // Usually romanised as "sha, shu, sho"
+  sya: 'シャ',
+  syu: 'シュ',
+  syo: 'ショ',
+
+  // Usually romanised as "cha, chu, cho"
+  tya: 'チャ',
+  tyu: 'チュ',
+  tyo: 'チョ',
+
+  nya: 'ニャ',
+  nyu: 'ニュ',
+  nyo: 'ニョ',
+
+  hya: 'ヒャ',
+  hyu: 'ヒュ',
+  hyo: 'ヒョ',
+
+  mya: 'ミャ',
+  myu: 'ミュ',
+  myo: 'ミョ',
+
+  rya: 'リャ',
+  ryu: 'リュ',
+  ryo: 'リョ',
+
+  // 濁点（だくてん）dakuten (diacritics).
+  gya: 'ギャ',
+  gyu: 'ギュ',
+  gyo: 'ギョ',
+
+  // Usually romanised as "ja, ju, jo"
+  zya: 'ジャ',
+  zyu: 'シュ',
+  zyo: 'ショ',
+
+  // Usually also romanised as "ja, ju, jo"
+  dya: 'ヂャ',
+  dyu: 'ヂュ',
+  dyo: 'ヂョ',
+
+  bya: 'ビャ',
+  byu: 'ビュ',
+  byo: 'ビョ',
+
+  // 半濁点　（はんだくてん）handakuten (diacritics)
+  pya: 'ピャ',
+  pyu: 'ピュ',
+  pyo: 'ピョ',
+
+  // End katakana.
+} as const
