37173e575580c32de4ff357669d095780ac5e657
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-19 19:23:31 -0300

    Add kana rows/groups.

---
+++ src/kana/kanas.ts
@@ -0,0 +1,551 @@
+import hiragana from './hiragana'
+import katakana from './katakana'
+
+export const kanaRows = {
+  // 五十音（ごじゅうおん）Monographs.
+  a: {
+    a: {
+      hg: hiragana.a,
+      kk: katakana.a,
+    },
+    i: {
+      hg: hiragana.i,
+      kk: katakana.i,
+    },
+    u: {
+      hg: hiragana.u,
+      kk: katakana.u,
+    },
+    e: {
+      hg: hiragana.e,
+      kk: katakana.e,
+    },
+    o: {
+      hg: hiragana.o,
+      kk: katakana.o,
+    },
+  },
+
+  ka: {
+    ka: {
+      hg: hiragana.ka,
+      kk: katakana.ka,
+    },
+    ki: {
+      hg: hiragana.ki,
+      kk: katakana.ki,
+    },
+    ku: {
+      hg: hiragana.ku,
+      kk: katakana.ku,
+    },
+    ke: {
+      hg: hiragana.ke,
+      kk: katakana.ke,
+    },
+    ko: {
+      hg: hiragana.ko,
+      kk: katakana.ko,
+    },
+  },
+
+  sa: {
+    sa: {
+      hg: hiragana.sa,
+      kk: katakana.sa,
+    },
+    si: {
+      hg: hiragana.si,
+      kk: katakana.si,
+    },
+    su: {
+      hg: hiragana.su,
+      kk: katakana.su,
+    },
+    se: {
+      hg: hiragana.se,
+      kk: katakana.se,
+    },
+    so: {
+      hg: hiragana.so,
+      kk: katakana.so,
+    },
+  },
+
+  ta: {
+    ta: {
+      hg: hiragana.ta,
+      kk: katakana.ta,
+    },
+    ti: {
+      hg: hiragana.ti,
+      kk: katakana.ti,
+    },
+    tu: {
+      hg: hiragana.tu,
+      kk: katakana.tu,
+    },
+    te: {
+      hg: hiragana.te,
+      kk: katakana.te,
+    },
+    to: {
+      hg: hiragana.to,
+      kk: katakana.to,
+    },
+  },
+
+  na: {
+    na: {
+      hg: hiragana.na,
+      kk: katakana.na,
+    },
+    ni: {
+      hg: hiragana.ni,
+      kk: katakana.ni,
+    },
+    nu: {
+      hg: hiragana.nu,
+      kk: katakana.nu,
+    },
+    ne: {
+      hg: hiragana.ne,
+      kk: katakana.ne,
+    },
+    no: {
+      hg: hiragana.no,
+      kk: katakana.no,
+    },
+  },
+
+  ha: {
+    ha: {
+      hg: hiragana.ha,
+      kk: katakana.ha,
+    },
+    hi: {
+      hg: hiragana.hi,
+      kk: katakana.hi,
+    },
+    hu: {
+      hg: hiragana.hu,
+      kk: katakana.hu,
+    },
+    he: {
+      hg: hiragana.he,
+      kk: katakana.he,
+    },
+    ho: {
+      hg: hiragana.ho,
+      kk: katakana.ho,
+    },
+  },
+
+  ma: {
+    ma: {
+      hg: hiragana.ma,
+      kk: katakana.ma,
+    },
+    mi: {
+      hg: hiragana.mi,
+      kk: katakana.mi,
+    },
+    mu: {
+      hg: hiragana.mu,
+      kk: katakana.mu,
+    },
+    me: {
+      hg: hiragana.me,
+      kk: katakana.me,
+    },
+    mo: {
+      hg: hiragana.mo,
+      kk: katakana.mo,
+    },
+  },
+
+  ya: {
+    ya: {
+      hg: hiragana.ya,
+      kk: katakana.ya,
+    },
+    yu: {
+      hg: hiragana.yu,
+      kk: katakana.yu,
+    },
+    yo: {
+      hg: hiragana.yo,
+      kk: katakana.yo,
+    },
+  },
+
+  ra: {
+    ra: {
+      hg: hiragana.ra,
+      kk: katakana.ra,
+    },
+    ri: {
+      hg: hiragana.ri,
+      kk: katakana.ri,
+    },
+    ru: {
+      hg: hiragana.ru,
+      kk: katakana.ru,
+    },
+    re: {
+      hg: hiragana.re,
+      kk: katakana.re,
+    },
+    ro: {
+      hg: hiragana.ro,
+      kk: katakana.ro,
+    },
+  },
+
+  wa: {
+    wa: {
+      hg: hiragana.wa,
+      kk: katakana.wa,
+    },
+    wo: {
+      hg: hiragana.wo,
+      kk: katakana.wo,
+    },
+    n: {
+      hg: hiragana.n,
+      kk: katakana.n,
+    },
+  },
+
+  // 濁点（だくてん）dakuten (diacritics).
+  ga: {
+    ga: {
+      hg: hiragana.ga,
+      kk: katakana.ga,
+    },
+    gi: {
+      hg: hiragana.gi,
+      kk: katakana.gi,
+    },
+    gu: {
+      hg: hiragana.gu,
+      kk: katakana.gu,
+    },
+    ge: {
+      hg: hiragana.ge,
+      kk: katakana.ge,
+    },
+    go: {
+      hg: hiragana.go,
+      kk: katakana.go,
+    },
+  },
+
+  za: {
+    za: {
+      hg: hiragana.za,
+      kk: katakana.za,
+    },
+    zi: {
+      hg: hiragana.zi,
+      kk: katakana.zi,
+    },
+    zu: {
+      hg: hiragana.zu,
+      kk: katakana.zu,
+    },
+    ze: {
+      hg: hiragana.ze,
+      kk: katakana.ze,
+    },
+    zo: {
+      hg: hiragana.zo,
+      kk: katakana.zo,
+    },
+  },
+
+  da: {
+    da: {
+      hg: hiragana.da,
+      kk: katakana.da,
+    },
+    di: {
+      hg: hiragana.di,
+      kk: katakana.di,
+    },
+    du: {
+      hg: hiragana.du,
+      kk: katakana.du,
+    },
+    de: {
+      hg: hiragana.de,
+      kk: katakana.de,
+    },
+    do: {
+      hg: hiragana.do,
+      kk: katakana.do,
+    },
+  },
+
+  ba: {
+    ba: {
+      hg: hiragana.ba,
+      kk: katakana.ba,
+    },
+    bi: {
+      hg: hiragana.bi,
+      kk: katakana.bi,
+    },
+    bu: {
+      hg: hiragana.bu,
+      kk: katakana.bu,
+    },
+    be: {
+      hg: hiragana.be,
+      kk: katakana.be,
+    },
+    bo: {
+      hg: hiragana.bo,
+      kk: katakana.bo,
+    },
+  },
+
+  // 半濁点　（はんだくてん）handakuten (diacritics)
+  pa: {
+    pa: {
+      hg: hiragana.pa,
+      kk: katakana.pa,
+    },
+    pi: {
+      hg: hiragana.pi,
+      kk: katakana.pi,
+    },
+    pu: {
+      hg: hiragana.pu,
+      kk: katakana.pu,
+    },
+    pe: {
+      hg: hiragana.pe,
+      kk: katakana.pe,
+    },
+    po: {
+      hg: hiragana.po,
+      kk: katakana.po,
+    },
+  },
+
+  // Soma aliases for other common romaji forms.
+  // Warning: Some conflicting forms are not being included.
+  // i.e. "ji" and "zu"
+  shi: {
+    hg: hiragana.shi,
+    kk: katakana.shi,
+  },
+  chi: {
+    hg: hiragana.chi,
+    kk: katakana.chi,
+  },
+  tsu: {
+    hg: hiragana.tsu,
+    kk: katakana.tsu,
+  },
+  fu: {
+    hg: hiragana.fu,
+    kk: katakana.fu,
+  },
+  ji: {
+    hg: hiragana.ji,
+    kk: katakana.ji,
+  },
+
+  // Digraphs.
+  // 五十音（ごじゅうおん）
+  kya: {
+    kya: {
+      hg: hiragana.kya,
+      kk: katakana.kya,
+    },
+    kyu: {
+      hg: hiragana.kyu,
+      kk: katakana.kyu,
+    },
+    kyo: {
+      hg: hiragana.kyo,
+      kk: katakana.kyo,
+    },
+  },
+
+  // Usually romanised as "sha, shu, sho"
+  sya: {
+    sya: {
+      hg: hiragana.sya,
+      kk: katakana.sya,
+    },
+    syu: {
+      hg: hiragana.syu,
+      kk: katakana.syu,
+    },
+    syo: {
+      hg: hiragana.syo,
+      kk: katakana.syo,
+    },
+  },
+
+  // Usually romanised as "cha, chu, cho"
+  tya: {
+    tya: {
+      hg: hiragana.tya,
+      kk: katakana.tya,
+    },
+    tyu: {
+      hg: hiragana.tyu,
+      kk: katakana.tyu,
+    },
+    tyo: {
+      hg: hiragana.tyo,
+      kk: katakana.tyo,
+    },
+  },
+
+  nya: {
+    nya: {
+      hg: hiragana.nya,
+      kk: katakana.nya,
+    },
+    nyu: {
+      hg: hiragana.nyu,
+      kk: katakana.nyu,
+    },
+    nyo: {
+      hg: hiragana.nyo,
+      kk: katakana.nyo,
+    },
+  },
+
+  hya: {
+    hya: {
+      hg: hiragana.hya,
+      kk: katakana.hya,
+    },
+    hyu: {
+      hg: hiragana.hyu,
+      kk: katakana.hyu,
+    },
+    hyo: {
+      hg: hiragana.hyo,
+      kk: katakana.hyo,
+    },
+  },
+
+  mya: {
+    mya: {
+      hg: hiragana.mya,
+      kk: katakana.mya,
+    },
+    myu: {
+      hg: hiragana.myu,
+      kk: katakana.myu,
+    },
+    myo: {
+      hg: hiragana.myo,
+      kk: katakana.myo,
+    },
+  },
+
+  rya: {
+    rya: {
+      hg: hiragana.rya,
+      kk: katakana.rya,
+    },
+    ryu: {
+      hg: hiragana.ryu,
+      kk: katakana.ryu,
+    },
+    ryo: {
+      hg: hiragana.ryo,
+      kk: katakana.ryo,
+    },
+  },
+
+  // 濁点（だくてん）dakuten (diacritics).
+  gya: {
+    gya: {
+      hg: hiragana.gya,
+      kk: katakana.gya,
+    },
+    gyu: {
+      hg: hiragana.gyu,
+      kk: katakana.gyu,
+    },
+    gyo: {
+      hg: hiragana.gyo,
+      kk: katakana.gyo,
+    },
+  },
+
+  // Usually romanised as "ja, ju, jo"
+  zya: {
+    zya: {
+      hg: hiragana.zya,
+      kk: katakana.zya,
+    },
+    zyu: {
+      hg: hiragana.zyu,
+      kk: katakana.zyu,
+    },
+    zyo: {
+      hg: hiragana.zyo,
+      kk: katakana.zyo,
+    },
+  },
+
+  // Usually also romanised as "ja, ju, jo"
+
+  dya: {
+    dya: {
+      hg: hiragana.dya,
+      kk: katakana.dya,
+    },
+    dyu: {
+      hg: hiragana.dyu,
+      kk: katakana.dyu,
+    },
+    dyo: {
+      hg: hiragana.dyo,
+      kk: katakana.dyo,
+    },
+  },
+
+  bya: {
+    bya: {
+      hg: hiragana.bya,
+      kk: katakana.bya,
+    },
+    byu: {
+      hg: hiragana.byu,
+      kk: katakana.byu,
+    },
+    byo: {
+      hg: hiragana.byo,
+      kk: katakana.byo,
+    },
+  },
+
+  // 半濁点　（はんだくてん）handakuten (diacritics)
+  pya: {
+    pya: {
+      hg: hiragana.pya,
+      kk: katakana.pya,
+    },
+    pyu: {
+      hg: hiragana.pyu,
+      kk: katakana.pyu,
+    },
+    pyo: {
+      hg: hiragana.pyo,
+      kk: katakana.pyo,
+    },
+  },
+
+  // End kana.
+} as const
