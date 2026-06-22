d3a6549b8182cd1bd491f0ad4abeca8ef0d93361
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-22 18:20:25 -0300

    Add unfoldable content to action buttons.

--- src/features/home/components/actionMenu.tsx
+++ src/features/home/components/actionMenu.tsx
@@ -1,25 +1,28 @@
 import type { kanaKind } from '../../../types/kana'
 import MenuButton from './menuButton'
 import './actionMenu.scss'
+import { useState } from 'react'
 
 interface actionMenuProps {
   kana: kanaKind
 }
 
 export default function ActionMenu({ kana }: actionMenuProps) {
+  const [open, setOpen] = useState<string | null>(null)
+
   return (
     <div id='action-menu'>
-      <MenuButton title='Cards' kana={kana} />
+      <MenuButton title='Cards' kana={kana} open={open} setOpen={setOpen} />
 
-      <MenuButton title='Chart' kana={kana} />
+      <MenuButton title='Chart' kana={kana} open={open} setOpen={setOpen} />
 
-      <MenuButton title='Drill' kana={kana} />
+      <MenuButton title='Drill' kana={kana} open={open} setOpen={setOpen} />
 
-      <MenuButton title='Help' kana={kana} />
+      <MenuButton title='Help' kana={kana} open={open} setOpen={setOpen} />
 
-      <MenuButton title='About' kana={kana} />
+      <MenuButton title='About' kana={kana} open={open} setOpen={setOpen} />
 
-      <MenuButton title='Coffee' kana={kana} />
+      <MenuButton title='Coffee' kana={kana} open={open} setOpen={setOpen} />
     </div>
   )
 }
--- src/features/home/components/menuButton.tsx
+++ src/features/home/components/menuButton.tsx
@@ -4,12 +4,29 @@ import './menuButton.scss'
 interface menuButtonProps {
   title: string
   kana: kanaKind // I'll use `kana` to send to the appropriate page.
+  open: string | null
+  setOpen: (s: string | null) => void
 }
 
-export default function MenuButton({ title }: menuButtonProps) {
+export default function MenuButton({ title, open, setOpen }: menuButtonProps) {
+  const handleClick = () => {
+    if (open === title) {
+      setOpen(null)
+    } else {
+      setOpen(title)
+    }
+  }
+
   return (
-    <div className='menu-button-outer'>
+    <div className='menu-button-outer' onClick={handleClick}>
       <div className='menu-button-title'>{title}</div>
+      <div
+        className={`menu-button-inner ${open === title ? 'open' : 'closed'}`}
+      >
+        <div className='menu-button-description'>This is a description.</div>
+
+        <div className='menu-button-go'>Go!</div>
+      </div>
     </div>
   )
 }
