b19e2b2af903c6a0a2599345ebbc5a958834e38f
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-22 18:39:20 -0300

    Add visual hint when menu buttons are scrolled up.

--- src/features/home/components/modeSelector.tsx
+++ src/features/home/components/modeSelector.tsx
@@ -9,30 +9,32 @@ export default function ModeSelector({
   setKana: (k: kanaKind) => void
 }) {
   return (
-    <div className='mode-selector'>
-      <div
-        className={`mode-outer-gradient ${kana === 'hiragana' ? 'left' : 'right'}`}
-      >
-        <div></div>
-      </div>
-
-      <div
-        className={`mode-hiragana-outer ${kana === 'hiragana' && 'active'}`}
-        onClick={() => setKana('hiragana')}
-      >
-        <div className={`mode-hiragana ${kana === 'hiragana' && 'active'}`}>
-          <div className='label-in-kana'>ひらがな</div>
-          <div className='label-in-romaji'>hiragana</div>
+    <div className='mode-selector-band'>
+      <div className='mode-selector'>
+        <div
+          className={`mode-outer-gradient ${kana === 'hiragana' ? 'left' : 'right'}`}
+        >
+          <div></div>
         </div>
-      </div>
 
-      <div
-        className={`mode-katakana-outer ${kana === 'katakana' && 'active'}`}
-        onClick={() => setKana('katakana')}
-      >
-        <div className={`mode-katakana ${kana === 'katakana' && 'active'}`}>
-          <div className='label-in-kana'>カタカナ</div>
-          <div className='label-in-romaji'>katakana</div>
+        <div
+          className={`mode-hiragana-outer ${kana === 'hiragana' && 'active'}`}
+          onClick={() => setKana('hiragana')}
+        >
+          <div className={`mode-hiragana ${kana === 'hiragana' && 'active'}`}>
+            <div className='label-in-kana'>ひらがな</div>
+            <div className='label-in-romaji'>hiragana</div>
+          </div>
+        </div>
+
+        <div
+          className={`mode-katakana-outer ${kana === 'katakana' && 'active'}`}
+          onClick={() => setKana('katakana')}
+        >
+          <div className={`mode-katakana ${kana === 'katakana' && 'active'}`}>
+            <div className='label-in-kana'>カタカナ</div>
+            <div className='label-in-romaji'>katakana</div>
+          </div>
         </div>
       </div>
     </div>
