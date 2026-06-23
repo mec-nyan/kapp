7894446aeee85e7bc71a58d660639009371a950d
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-23 18:10:22 -0300

    Make TopBar and Footer present on every route.

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.scss'
 import { Home } from './pages/home'
+import TopBar from './features/shared/components/topBar'
+import Footer from './features/shared/components/footer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <TopBar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   </StrictMode>
 )
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,10 @@
 import './home.scss'
-import TopBar from '../features/shared/components/topBar'
-import Footer from '../features/shared/components/footer'
 import MainMenu from '../features/home/components/mainMenu'
 
 export function Home() {
   return (
     <div id='main-container'>
-      <TopBar />
-
       <MainMenu />
-
-      <Footer />
     </div>
   )
 }
