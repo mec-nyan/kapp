26ebcf3385f9e57c6f1631c9d5828b51b7147a16
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-23 18:03:21 -0300

    Add routing.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.scss'
 import { Home } from './pages/home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
