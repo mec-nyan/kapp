dcd46b13f8ddbb06224b01ec47b949c68c548bb9
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-25 17:47:20 -0300

    Add basic structure for examples.

---
+++ src/kana/examples.ts
@@ -0,0 +1,764 @@
+import { type Example } from '../types/kana'
+
+export default {
+  // TODO: Add examples for diacritycs and digraphs.
+  hiragana: new Map<string, Example>([
+    [
+      'a',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'i',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'u',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'e',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'o',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ka',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ki',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ku',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ke',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ko',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'sa',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'si',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'su',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'se',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'so',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ta',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ti',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'tu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'te',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'to',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'na',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ni',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'nu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ne',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'no',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ha',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'hi',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'hu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'he',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ho',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ma',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'mi',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'mu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'me',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'mo',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ya',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'yu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'yo',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ra',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ri',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ru',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      're',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ro',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'wa',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'wo',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'n',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+  ]),
+
+  katakana: new Map<string, Example>([
+    [
+      'a',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'i',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'u',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'e',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'o',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ka',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ki',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ku',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ke',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ko',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'sa',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'si',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'su',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'se',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'so',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ta',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ti',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'tu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'te',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'to',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'na',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ni',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'nu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ne',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'no',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ha',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'hi',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'hu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'he',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ho',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ma',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'mi',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'mu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'me',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'mo',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ya',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'yu',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'yo',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'ra',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ri',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ru',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      're',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'ro',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+
+    [
+      'wa',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'wo',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+    [
+      'n',
+      {
+        jap: '',
+        rmj: '',
+        picture: '',
+      },
+    ],
+  ]),
+} as const
