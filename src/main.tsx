a11eb1c3a7972e6ccae490555d970e3477586614
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-07-04 22:07:59 -0300

    PWA: Configure build options and routing.

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ registerServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <TopBar />
       <Routes>
         <Route path='/' element={<Home />} />
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kapp/',
+  build: {
+    copyPublicDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/index.js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: (info) => {
+          if (info.names.includes('styles.css')) {
+            return 'index.css'
+          }
+          return 'assets/[name][extname]'
+        },
+      },
+    },
+  },
 })
