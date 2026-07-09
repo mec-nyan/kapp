166e3cc85251a7269533536a6c0039e363078aed
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-07-08 21:08:59 -0300

    Add hiragana to romaji table.

--- src/kana/hiragana.ts
+++ src/kana/hiragana.ts
@@ -155,3 +155,156 @@ export default {
 
   // End Hiragana.
 } as const
+
+// TODO: Romaji is not a perfect system.  I've chosen a popular form of romanization based on the
+// "approximation" (???) of the sound.  So, there is some ambiguity here.  I need to find a better
+// way.  Hopefully, one that's not so far from the sound but also can be "typed" i.e. within an IME
+// so it results in the right hiragana.
+export const hiraganaToRomaji = new Map<string, string>([
+  // 五十音（ごじゅうおん）Monographs.
+  ['あ', 'a'],
+  ['い', 'i'],
+  ['う', 'u'],
+  ['え', 'e'],
+  ['お', 'o'],
+
+  ['か', 'ka'],
+  ['き', 'ki'],
+  ['く', 'ku'],
+  ['け', 'ke'],
+  ['こ', 'ko'],
+
+  ['さ', 'sa'],
+  ['し', 'shi'],
+  ['す', 'su'],
+  ['せ', 'se'],
+  ['そ', 'so'],
+
+  ['た', 'ta'],
+  ['ち', 'chi'],
+  ['つ', 'tsu'],
+  ['て', 'te'],
+  ['と', 'to'],
+
+  ['な', 'na'],
+  ['に', 'ni'],
+  ['ぬ', 'nu'],
+  ['ね', 'ne'],
+  ['の', 'no'],
+
+  ['は', 'ha'],
+  ['ひ', 'hi'],
+  ['ふ', 'fu'],
+  ['へ', 'he'],
+  ['ほ', 'ho'],
+
+  ['ま', 'ma'],
+  ['み', 'mi'],
+  ['む', 'mu'],
+  ['め', 'me'],
+  ['も', 'mo'],
+
+  ['や', 'ya'],
+  ['ゆ', 'yu'],
+  ['よ', 'yo'],
+
+  ['ら', 'ra'],
+  ['り', 'ri'],
+  ['る', 'ru'],
+  ['れ', 're'],
+  ['ろ', 'ro'],
+
+  ['わ', 'wa'],
+  ['を', 'o'],
+  ['ん', 'n'],
+
+  // 濁点（だくてん）dakuten (diacritics).
+  ['が', 'ga'],
+  ['ぎ', 'gi'],
+  ['ぐ', 'gu'],
+  ['げ', 'ge'],
+  ['ご', 'go'],
+
+  ['ざ', 'za'],
+  ['じ', 'ji'],
+  ['ず', 'zu'],
+  ['ぜ', 'ze'],
+  ['ぞ', 'zo'],
+
+  ['だ', 'da'],
+  ['ぢ', 'ji'], // TODO: how to disambiguate this from previous 'ji'...
+  ['づ', 'zu'], // id.
+  ['で', 'de'],
+  ['ど', 'do'],
+
+  ['ば', 'ba'],
+  ['び', 'bi'],
+  ['ぶ', 'bu'],
+  ['べ', 'be'],
+  ['ぼ', 'bo'],
+
+  // 半濁点　（はんだくてん）handakuten (diacritics)
+  ['ぱ', 'pa'],
+  ['ぴ', 'pi'],
+  ['ぷ', 'pu'],
+  ['ぺ', 'pe'],
+  ['ぽ', 'po'],
+
+  // Digraphs.
+  // 五十音（ごじゅうおん）
+  ['きゃ', 'kya'],
+  ['きゅ', 'kyu'],
+  ['きょ', 'kyo'],
+
+  // Usually romanised as "sha, shu, sho"
+  ['しゃ', 'sha'],
+  ['しゅ', 'shu'],
+  ['しょ', 'sho'],
+
+  // Usually romanised as "cha, chu, cho"
+  ['ちゃ', 'cha'],
+  ['ちゅ', 'chu'],
+  ['ちょ', 'cho'],
+
+  ['にゃ', 'nya'],
+  ['にゅ', 'nyu'],
+  ['にょ', 'nyo'],
+
+  ['ひゃ', 'hya'],
+  ['ひゅ', 'hyu'],
+  ['ひょ', 'hyo'],
+
+  ['みゃ', 'mya'],
+  ['みゅ', 'myu'],
+  ['みょ', 'myo'],
+
+  ['りゃ', 'rya'],
+  ['りゅ', 'ryu'],
+  ['りょ', 'ryo'],
+
+  // 濁点（だくてん）dakuten (diacritics).
+  ['ぎゃ', 'gya'],
+  ['ぎゅ', 'gyu'],
+  ['ぎょ', 'gyo'],
+
+  // Usually romanised as "ja, ju, jo"
+  ['じゃ', 'ja'],
+  ['じゅ', 'ju'],
+  ['じょ', 'jo'],
+
+  // Usually also romanised as "ja, ju, jo"
+  ['ちゃ', 'ja'],
+  ['ちゅ', 'ju'],
+  ['ちょ', 'jo'],
+
+  ['びゃ', 'bya'],
+  ['びゅ', 'byu'],
+  ['びょ', 'byo'],
+
+  // 半濁点　（はんだくてん）handakuten (diacritics)
+  ['ぴゃ', 'pya'],
+  ['ぴゅ', 'pyu'],
+  ['ぴょ', 'pyo'],
+
+  // End Hiragana.
+])
