2a4cbe12759ca3ae16ebc731c53533bac89755ce
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-07-10 15:20:17 -0300

    Add languageSelector component.

---
+++ src/features/shared/components/languageSelector.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import './languageSelector.css'
+import { useLanguage } from '../../../hooks/useLanguage'
+import type { Lang, LanguageField } from '../../../types/lang'
+
+export default function LanguageSelector() {
+  const { i18n } = useTranslation()
+  const { lang, setLang } = useLanguage()
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const handleLanguageChange = (newLang: Lang) => {
+    setLang(newLang)
+    i18n.changeLanguage(newLang)
+    setIsOpen(false)
+  }
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const languages: LanguageField[] = [
+    {
+      name: 'English',
+      flag: '🇬🇧',
+      abbr: 'en',
+    },
+    {
+      name: 'Español',
+      flag: '🇪🇸',
+      abbr: 'es',
+    },
+  ]
+
+  const popupFields = languages.map((field) => {
+    return (
+      <div onClick={() => handleLanguageChange(field.abbr)}>
+        <span className='flag'>{field.flag}</span>
+        <span className='current-lang'>{lang === field.abbr && '•'}</span>
+        <span className={`lang-name ${lang === field.abbr ? 'current' : ''}`}>
+          {field.name}
+        </span>
+      </div>
+    )
+  })
+
+  return (
+    <div className='lang-switcher' onClick={handleToggle}>
+      <span className='material-symbols-outlined'>translate</span>
+
+      {isOpen && <div className='lang-popup'>{popupFields}</div>}
+    </div>
+  )
+}
