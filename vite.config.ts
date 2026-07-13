ad11df598c14c1d34f8ea4eac80f1a4c6262d306
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-07-13 20:07:46 -0300

    Sync app version globally.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
   base: '/kapp/',
   build: {
     copyPublicDir: true,
