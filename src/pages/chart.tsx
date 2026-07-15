dd0af155be9e1e47aff055968e00ba9690035627
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-07-14 21:43:30 -0300

    Add first (hiragana) chart.

--- src/pages/chart.tsx
+++ src/pages/chart.tsx
@@ -1,20 +1,50 @@
 import { NavLink } from 'react-router'
 import './chart.css'
 import type { kanaKind } from '../types/kana'
+import KanaTable from '../kana/kanas'
 import { inKana } from '../kana/kanas'
+import { toRomajiFormmated } from '../kana/romaji'
 
 interface ChartProps {
   kana: kanaKind
 }
 
 export default function Chart({ kana }: ChartProps) {
+  const keys = [...KanaTable.keys()]
+
+  const rows = []
+
+  for (const key of keys) {
+    const row = []
+    const kanas = KanaTable.get(key)!.Basic.Monographs
+    for (const kr of kanas) {
+      if (kr) {
+        row.push(
+          <div className='kana-cell'>
+            <div className='kana-line'>
+              <div className='hiragana'>{kr.hg}</div>
+              <div className='katakana'>{kr.kk}</div>
+            </div>
+            <div className='picture'>{kr.examples.hg.picture}</div>
+            <div className='word'>{kr.examples.hg.jap}</div>
+            <div className='romaji'>
+              {toRomajiFormmated(kr.examples.hg.jap)}
+            </div>
+          </div>
+        )
+      } else {
+        row.push(<div className='kana empty'></div>)
+      }
+    }
+    rows.push(<div className='row'>{row}</div>)
+  }
   return (
     <div id='chart-container'>
       <div className='chart-title'>
         {inKana(kana)} . {kana.charAt(0).toUpperCase() + kana.slice(1)}
       </div>
 
-      <div className='chart'>We'll draw the hiragana/katakana chart here.</div>
+      <div className='chart'>{rows}</div>
 
       <NavLink to='/'>
         <div className='back-button'>
